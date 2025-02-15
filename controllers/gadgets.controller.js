59b5df718308ef5dbbedcfa3f3905a1ff4b59a2b
Prashant Mishra <prashantmishra10232@gmail.com>
2025-02-15 19:52:45 +0530

    added the controllers

---
+++ controllers/gadgets.controller.js
@@ -0,0 +1,126 @@
+import { asyncHandler } from "../utils/AsyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { Gadget } from "../models/gadgets.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { v4 as uuidv4 } from "uuid";
+
+//function to generate random codenames for gadgets
+const generateRandomCodeName = () => {
+  const names = ["The Nightingale", "The Kraken", "The Dragon", "The Wolf"];
+  return names[Math.floor(Math.random() * names.length)];
+};
+
+//function to generate mission success probability
+
+const createGadget = asyncHandler(async (req, res) => {
+  const gadget = await Gadget.create({
+    id: uuidv4(),
+    name: generateRandomCodeName(),
+  });
+
+  if (!gadget) {
+    throw new ApiError(500, "Error adding gadget");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, gadget, "New gadget added successfully"));
+});
+
+const getAllGadgets = asyncHandler(async (req, res) => {
+  const gadgets = await Gadget.aggregate([
+    {
+      $addFields: {
+        missionSuccessProbability: {
+          $floor: { $multiply: [{ $rand: {} }, 100] },
+        }, //will generate random probability
+      },
+    },
+  ]);
+
+  if (!gadgets || gadgets.length === 0) {
+    throw new ApiError(404, "No gadgets found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, gadgets, "All Gadgets retrieved successfully"));
+});
+
+const updateDetails = asyncHandler(async (req, res) => {
+  const gadgetId = req.params;
+
+  const { name, status } = req.body;
+
+  const allowedStatus = ["Deployed", "Destroyed", "Decommissioned"];
+  if (status && !allowedStatus.includes(role)) {
+    throw new ApiError(400, "Invalid Status provided");
+  }
+
+  const gadget = await Gadget.findByIdAndUpdate(gadgetId, 
+    req.body,
+    { name, status },
+  {
+    new: true,
+    runValidators: true,
+  }
+);
+
+  if (!gadget) {
+    throw new ApiError(404, "Gadget not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, gadget, "Gadget updated successfully"));
+});
+
+const deleteGadget = asyncHandler(async(req,res)=>{
+    const gadgetId = req.params;
+
+    const gadget = await Gadget.findByIdAndUpdate(
+        gadgetId,
+        {
+            status: "Decommisioned",
+            decommisionedAt: new Date(),
+        },
+        {
+            new:true,
+            runValidators:true
+        }
+    )
+
+    if(!gadget){
+        throw new ApiError(404,"No Gadget Found")
+    }
+
+    return res.status(200
+        .json(new ApiResponse(200,gadget,"Gadget deleted successfully"))
+    )
+})
+
+const selfDestruct = asyncHandler(async(req,res)=>{
+    const gadgetId = req.params;
+
+    //6 digit random code
+    const confirmationCode = Math.floor(100000 + Math.random()*900000).toString();
+
+    const gadget = await Gadget.findByIdAndUpdate(
+        gadgetId,
+        {
+            status: "Destroyed",
+            selfDestructAt: new Date()
+        },
+        {
+            new:true,
+            runValidators:true
+        }
+    );
+    if(!gadget){
+        throw new ApiError(404, "Gadget not found");
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200,{gadget,confirmationCode},"Self-destruct sequence initialized"))
+})
+export { createGadget, getAllGadgets ,updateDetails,deleteGadget,selfDestruct};
---
+++ controllers/user.controller.js
@@ -0,0 +1,202 @@
+import { asyncHandler } from "../utils/AsyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
+
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken; //assigning the refreshToken to the user object
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "something went wrong while generating refresh and access token"
+    );
+  }
+};
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { userName, fullName, email, password } = req.body;
+
+  if (
+    [userName, fullName, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = await User.findOne({
+    $or: [{ userName }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or userName already existed");
+  }
+
+  const user = await User.create({
+    userName,
+    fullName,
+    email,
+    password,
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, "User registered successfully"));
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!(email || password)) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    throw new ApiError(400, "User does not exist");
+  }
+
+  //checkin if the password is correct
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in successfully"
+      )
+    );
+});
+
+const logOut = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: {
+        refreshToken: 1, //removes the field from the document
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"));
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  const decodedToken = jwt.verify(
+    incomingRefreshToken,
+    process.env.REFRESH_TOKEN_SECRET
+  );
+
+  const user = await User.findById(decodedToken?._id);
+
+  if (!user) {
+    throw new ApiError(401, "Invalid refresh token");
+  }
+
+  if (incomingRefreshToken !== user?.refreshToken) {
+    throw new ApiError(401, "RefreshToken is expired or used");
+  }
+
+  const { accessToken, newRefreshToken } =
+    await User.generateAccessAndRefreshToken(user._id);
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", newRefreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { accessToken, newRefreshToken },
+        "Access Token refreshed"
+      )
+    );
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName: fullName,
+        email: email,
+      },
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { user }, "User deatils updated successfully"));
+});
+
+export { registerUser, logOut, loginUser, refreshAccessToken, updateUserProfile };
